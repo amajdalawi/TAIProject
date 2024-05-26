c4e313c43be358c82cc5408abeb24d8efe121de8
Abudlrahman Almajdalawi <at.majdalawi@gmail.com>
2024-05-26 17:33:45 +0200

    add pipelines, CV and GridSearch

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ This is the first commit of the readme
 
 Stages of project will be:
 1. Prepare dataset by merging all the training data csv files [DONE]
-2. Prepare the pipelines
-3. Define the metrics used to analyze the data (MSE)
-4. Define a few parameters for finding the best hyperparameters (for KNN, RandomForset, MLP, SVM)
-5. Do the pipelines and the gridsearchCV with TimeSeriesSplit as cv
+2. Prepare the pipelines [ALMOST DONE]
+3. Define the metrics used to analyze the data (MSE) [ALMOST DONE]
+4. Define a few parameters for finding the best hyperparameters (for KNN, RandomForset, MLP, SVM) [ALMOST DONE]
+5. Do the pipelines and the gridsearchCV with TimeSeriesSplit as cv [DONE]
 6. For each "best" estimator model with the best hyperparameters, run the estimators on the final test set (last week of april)
 7. Plot stuff up
 
--- run_main.py
+++ run_main.py
@@ -1,10 +1,69 @@
 import sklearn
+import sklearn.pipeline
 from prepare_datasets import get_pruned_df
 from pprint import pprint
 from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+# Models to use on dataset
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
+
+
+import pandas as pd
 
 prepared_df = get_pruned_df()
 # pprint(prepared_df)
+X = prepared_df.drop(columns=['real_temp'])
+Y = prepared_df['real_temp']
+# make test data as last week
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y , shuffle=False, random_state=0, test_size=1047)
+# pprint(X_train)
 
-X_train, X_test, Y_train, Y_test = train_test_split(prepared_df.drop(columns=['real_temp']), prepared_df['real_temp'], shuffle=False, random_state=0, test_size=1000)
-pprint(X_train)
+# columns in the dataframe to scale their values
+columns_to_scale = ['cape','sp','tcw','sshf','slhf','msl','u10','v10','d2m','ssr','str','ttr','sund','sm','st','sd','sf','tcc','tp','mx2t6','mn2t6']
+
+# Time-series-based cross-validator
+ts_cv = TimeSeriesSplit(n_splits=20)
+
+column_scaler = ColumnTransformer(
+        transformers=[
+            ("To_scale",StandardScaler(),columns_to_scale)
+        ]
+    )
+
+# Linear (Ridge) Regression Pipeline 
+ridge_pipeline = make_pipeline(
+    column_scaler,
+    GridSearchCV(estimator=Ridge(),param_grid={
+        'alpha':[0,0.3,0.5,1.0,1.3,3,5]
+    },scoring=["neg_mean_squared_error"],refit='neg_mean_squared_error', cv=ts_cv)
+)
+
+# K-NN Regressor Pipeline
+knn_pipeline = make_pipeline(
+    column_scaler,
+    GridSearchCV(KNeighborsRegressor(), param_grid={
+        'n_neighbors': [5,6,7,8,9,10]
+    }, scoring=['neg_mean_squared_error'], refit='neg_mean_squared_error', cv=ts_cv)
+)
+
+# Multiple Layer Perceptron Regressor
+mlp_pipeline = make_pipeline(
+    ColumnTransformer(MinMaxScaler()),
+    
+)
+
+
+ridge_pipeline.fit(X_train, Y_train)
+result = ridge_pipeline.predict(X_test)
+
+knn_regressor = knn_pipeline.fit(X_train, Y_train)
+results = knn_regressor.predict(X_test)
+pprint(results)
+# pd.DataFrame(result).to_csv("result.csv")
